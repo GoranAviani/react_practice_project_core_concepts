83d151280daeaca5e6ff20f625d9a6499f4d13cf
GoranAviani <goranaviani@gmail.com>
2022-04-25 19:11:27 +0200

    react_practice_project_core_concepts

--- src/App.js
+++ src/App.js
@@ -1,8 +1,14 @@
 import React from "react";
 
+import AddUser from "./Users/AddUser";
+
 const App = () => {
 
-    return <div>App</div>
+    return (
+        <div>
+        <AddUser/>
+    </div>
+    )
 }
 
 export default App;
