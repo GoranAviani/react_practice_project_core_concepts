95e43741fdadb227a0c3e28520047f66db4e0f5c
GoranAviani <goranaviani@gmail.com>
2022-04-25 20:22:28 +0200

    react_practice_project_core_concepts

---
+++ src/UI/ErrorModal.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const ErrorModal = () => {
+    return (
+        <React.Fragment>
+            error
+        </React.Fragment>
+    )
+}
+
+export default ErrorModal;
