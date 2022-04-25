2055d50c6ed800e829017b6220a9e7182f3382da
GoranAviani <goranaviani@gmail.com>
2022-04-25 19:24:43 +0200

    react_practice_project_core_concepts

--- src/App.js
+++ src/App.js
@@ -3,10 +3,17 @@ import React , {useState} from "react";
 import AddUser from "./Users/AddUser";
 
 const App = () => {
+    const [userList, setUserList] = useState([])
+
+    const expandUserList = (userName) => {
+        setUserList([...userList, userName])
+                console.log('APP' + userList)
+
+    }
 
     return (
         <div>
-        <AddUser/>
+        <AddUser addUser={expandUserList}/>
     </div>
     )
 }
