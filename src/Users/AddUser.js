adba9f9399103d695d177ebbc752f380fab3d764
GoranAviani <goranaviani@gmail.com>
2022-04-25 19:24:40 +0200

    react_practice_project_core_concepts

--- src/Users/AddUser.js
+++ src/Users/AddUser.js
@@ -1,18 +1,27 @@
 import React , {useState} from "react";
 
-const AddUser = () => {
+const AddUser = (props) => {
     const [userName, setUserName] = useState('')
 
     const userNameHandler = (e)=> {
         setUserName(e.target.value)
-        console.log(userName)
+    }
+
+
+    const submitHandler = (e) => {
+        e.preventDefault()
+        props.addUser(userName)
+
     }
 
     return (
+        <form onSubmit={submitHandler}>
         <div>
             <input value={userName} onChange={userNameHandler}/>
             add user
+            <button type='submit' >Add User</button>
         </div>
+            </form>
     )
 }
 
