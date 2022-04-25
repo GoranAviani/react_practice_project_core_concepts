93a4768fc6fdbde2030106da36866de65ba1c5be
GoranAviani <goranaviani@gmail.com>
2022-04-25 19:15:02 +0200

    react_practice_project_core_concepts

--- src/Users/AddUser.js
+++ src/Users/AddUser.js
@@ -1,8 +1,16 @@
-import React from "react";
+import React , {useState} from "react";
 
 const AddUser = () => {
+    const [userName, setUserName] = useState('')
+
+    const userNameHandler = (e)=> {
+        setUserName(e.target.value)
+        console.log(userName)
+    }
+
     return (
         <div>
+            <input value={userName} onChange={userNameHandler}/>
             add user
         </div>
     )
